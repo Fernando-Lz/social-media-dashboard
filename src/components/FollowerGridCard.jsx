3c12b520d0f0dc138126308fe0a408ff37e18f7f
Fernando <flopezlopez6@outlook.com>
2022-05-21 16:29:25 -0500

    :lipstick: Add basic style to followers card

--- src/components/FollowerGridCard.jsx
+++ src/components/FollowerGridCard.jsx
@@ -3,22 +3,24 @@ import PropTypes from 'prop-types';
 const FollowerGridCard = ({
   accountName, followers, dailyNewFollowers, platform,
 }) => {
-  const logo = `/src/assets/icon-${platform.toLowerCase()}.svg`;
+  const logo = `/src/assets/icon-${platform}.svg`;
   const newFollowersIcon = (dailyNewFollowers >= 0) ? '/src/assets/icon-up.svg' : '/src/assets/icon-down.svg';
+
   return (
-    <div className="account-card">
+    <div className={`account-card ${platform}`}>
       <div className="account-name">
         <img src={logo} alt={`${platform} logo`} />
         <h2>{accountName}</h2>
       </div>
-      <p className="account-followers">{followers}</p>
-      <p>FOLLOWERS</p>
+      <div className="account-followers">
+        {followers}
+        <p>FOLLOWERS</p>
+      </div>
       <p className="account-followers-status">
         <img src={newFollowersIcon} alt="Status logo" />
         {' '}
         {dailyNewFollowers}
       </p>
-      <hr />
     </div>
 
   );
--- src/components/FollowersGrid.jsx
+++ src/components/FollowersGrid.jsx
@@ -1,26 +1,23 @@
 import accounts from '../data/followers';
 import FollowerGridCard from './FollowerGridCard';
 
-const FollowersGrid = () => {
-  console.log(accounts);
-  return (
-    <div className="container-followers">
-      {accounts.map((account) => {
-        const {
-          accountName, followers, dailyNewFollowers, platform,
-        } = account;
-        return (
-          <FollowerGridCard
-            key={platform}
-            accountName={accountName}
-            followers={followers}
-            dailyNewFollowers={dailyNewFollowers}
-            platform={platform}
-          />
-        );
-      })}
-    </div>
-  );
-};
+const FollowersGrid = () => (
+  <div className="container-followers">
+    {accounts.map((account) => {
+      const {
+        accountName, followers, dailyNewFollowers, platform,
+      } = account;
+      return (
+        <FollowerGridCard
+          key={platform}
+          accountName={accountName}
+          followers={followers}
+          dailyNewFollowers={dailyNewFollowers}
+          platform={platform.toLowerCase()}
+        />
+      );
+    })}
+  </div>
+);
 
 export default FollowersGrid;
